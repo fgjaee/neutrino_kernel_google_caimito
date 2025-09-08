BayLibre/android_kernel_common

ANDROID: virt: gunyah: Add addrspace_find_info_area hypercall
Newer versions of Gunyah provide a mechanism to discover early-boot
configuration information. This information was previously
non-discoverable and was provided via devicetree patching by Gunyah. The
info area is a page allocated by the hypervisor and mapped into the
guest.

Bug: 395833312
Change-Id: I3dd4781f163d72c3826fcb4c354e4deda865efab
Signed-off-by: Elliot Berman <elliot.berman@oss.qualcomm.com>

arch/arm64/gunyah/gunyah_hypercall.c
@@ -45,6 +45,7 @@ EXPORT_SYMBOL_GPL(arch_is_gunyah_guest);
 #define GUNYAH_HYPERCALL_ADDRSPACE_UNMAP	GUNYAH_HYPERCALL(0x802C)
 #define GUNYAH_HYPERCALL_MEMEXTENT_DONATE	GUNYAH_HYPERCALL(0x8061)
 #define GUNYAH_HYPERCALL_VCPU_RUN		GUNYAH_HYPERCALL(0x8065)
+#define GUNYAH_HYPERCALL_ADDRSPACE_FIND_INFO_AREA	GUNYAH_HYPERCALL(0x806a)
 /* clang-format on */
 
 /**
@@ -274,6 +275,29 @@ gunyah_hypercall_vcpu_run(u64 capid, unsigned long *resume_data,
 	return res.a0;
 }
 EXPORT_SYMBOL_GPL(gunyah_hypercall_vcpu_run);
+
+/**
+ * gunyah_hypercall_addrspace_find_info_area() - Find the IPA and size of the info area
+ * @ipa: Filled with the IPA of the info area
+ * @size: Filled with the size of the info area
+ *
+ * See also:
+ * https://github.com/quic/gunyah-hypervisor/blob/develop/docs/api/gunyah_api.md#address-space-management
+ */
+enum gunyah_error
+gunyah_hypercall_addrspace_find_info_area(unsigned long *ipa, unsigned long *size)
+{
+	struct arm_smccc_res res;
+
+	arm_smccc_1_1_hvc(GUNYAH_HYPERCALL_ADDRSPACE_FIND_INFO_AREA, 0, &res);
+	if (res.a0 == GUNYAH_ERROR_OK) {
+		*ipa = res.a1;
+		*size = res.a2;
+	}
+
+	return res.a0;
+}
+EXPORT_SYMBOL_GPL(gunyah_hypercall_addrspace_find_info_area);
 
 MODULE_LICENSE("GPL");
 MODULE_DESCRIPTION("Gunyah Hypervisor Hypercalls");

include/linux/gunyah.h
@@ -480,4 +480,7 @@ enum gunyah_error
 gunyah_hypercall_vcpu_run(u64 capid, unsigned long *resume_data,
 			  struct gunyah_hypercall_vcpu_run_resp *resp);
 
+enum gunyah_error
+gunyah_hypercall_addrspace_find_info_area(unsigned long *ipa, unsigned long *size);
+
 #endif

fgjaee/neutrino_kernel_google_caimito

Bug: 438364674
Signed-off-by: Hrishabh Rajput <quic_hrishabh@quicinc.com>

arch/arm64/gunyah/gunyah_hypercall.c
@@ -40,6 +40,7 @@ EXPORT_SYMBOL_GPL(arch_is_gh_guest);
 #define GH_HYPERCALL_MSGQ_SEND			GH_HYPERCALL(0x801B)
 #define GH_HYPERCALL_MSGQ_RECV			GH_HYPERCALL(0x801C)
 #define GH_HYPERCALL_VCPU_RUN			GH_HYPERCALL(0x8065)
+#define GH_HYPERCALL_ADDRSPACE_FIND_INFO_AREA	GH_HYPERCALL(0x806A)
 
 /**
  * gh_hypercall_hyp_identify() - Returns build information and feature flags
@@ -137,6 +138,29 @@ enum gh_error gh_hypercall_vcpu_run(u64 capid, u64 *resume_data,
 	return res.a0;
 }
 EXPORT_SYMBOL_GPL(gh_hypercall_vcpu_run);
+
+/**
+ * gh_hypercall_addrspace_find_info_area() - Find the IPA and size of the info area
+ * @ipa: Filled with the IPA of the info area
+ * @size: Filled with the size of the info area
+ *
+ * See also:
+ * https://github.com/quic/gunyah-hypervisor/blob/develop/docs/api/gunyah_api.md#address-space-management
+ */
+enum gh_error
+gh_hypercall_addrspace_find_info_area(unsigned long *ipa, unsigned long *size)
+{
+	struct arm_smccc_res res;
+
+	arm_smccc_1_1_hvc(GH_HYPERCALL_ADDRSPACE_FIND_INFO_AREA, 0, &res);
+	if (res.a0 == GH_ERROR_OK) {
+		*ipa = res.a1;
+		*size = res.a2;
+	}
+
+	return res.a0;
+}
+EXPORT_SYMBOL_GPL(gh_hypercall_addrspace_find_info_area);
 
 MODULE_LICENSE("GPL");
 MODULE_DESCRIPTION("Gunyah Hypervisor Hypercalls");

include/linux/gunyah.h
@@ -204,4 +204,7 @@ struct gh_hypercall_vcpu_run_resp {
 enum gh_error gh_hypercall_vcpu_run(u64 capid, u64 *resume_data,
 					struct gh_hypercall_vcpu_run_resp *resp);
 
+enum gh_error
+gh_hypercall_addrspace_find_info_area(unsigned long *ipa, unsigned long *size);
+
 #endif